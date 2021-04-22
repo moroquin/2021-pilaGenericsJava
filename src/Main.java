ac367406a7590782a2d6cb177e633067045b60e0
Oliver Sierra Pac <sierra.oe@gmail.com>
2021-04-22 10:15:06 -0600

    hola mundo con ant

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+package src;
+
+/**
+ * @author oliver sierra
+ * @version 0.0
+ * 
+ * Main
+ */
+public class Main {
+
+    /**
+     * método principal
+     * @param args
+     */
+    public static void main(String[] args){
+        System.out.println("hola mundo con ant");
+    }
+}
